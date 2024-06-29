6dbd4428d6e13aa4824a4245aacf20f5663818bb
Omar <oabuhammoud@gmail.com>
2024-06-29 19:57:14 -0400

    Began the tutorial level. Implemented 'Chrono Objects.'

--- scripts/level_menu.gd
+++ scripts/level_menu.gd
@@ -3,4 +3,4 @@ extends Control
 
 
 func _on_tutorial_pressed():
-	get_tree().change_scene_to_file("res://scenes/levels/template.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/tutorial.tscn")
